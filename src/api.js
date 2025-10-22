54909060e8fdeeeca2798e6b7edc21bed2b7b8b8
Mochendra <hendraadmaja946@gmmail.com>
2025-10-23 03:20:31 +0700

    FE update api.js perbaikan

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5000/api',
-  baseURL: 'https://bejavis-production.up.railway.app//api',
+  baseURL: 'https://bejavis-production.up.railway.app/api',
   withCredentials: true
 });
 
